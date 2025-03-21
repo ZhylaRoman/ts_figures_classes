e7f57416ba875ad193bb1e4eda241411a4e3d511
Roman Zhyla <zhyla.roman25@gmail.com>
2025-03-21 10:55:17 +0100

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -7,19 +7,11 @@ export interface Figure {
 export class Triangle implements Figure {
   shape: string = 'triangle';
 
-  public color: 'red' | 'green' | 'blue';
-
-  private a: number;
-
-  private b: number;
-
-  private c: number;
-
   public constructor(
-    color: 'red' | 'green' | 'blue',
-    a: number,
-    b: number,
-    c: number,
+    public color: 'red' | 'green' | 'blue',
+    private a: number,
+    private b: number,
+    private c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Sides must be greater than 0');
@@ -28,11 +20,6 @@ export class Triangle implements Figure {
     if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error('Invalid triangle sides');
     }
-
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   public getArea(): number {
@@ -46,17 +33,13 @@ export class Triangle implements Figure {
 export class Circle implements Figure {
   shape: string = 'circle';
 
-  public color: 'red' | 'green' | 'blue';
-
-  private radius: number;
-
-  constructor(color: 'red' | 'green' | 'blue', radius: number) {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    private radius: number,
+  ) {
     if (radius <= 0) {
       throw new Error('Radius must be greater than 0');
     }
-
-    this.color = color;
-    this.radius = radius;
   }
 
   public getArea(): number {
@@ -69,20 +52,14 @@ export class Circle implements Figure {
 export class Rectangle implements Figure {
   shape: string = 'rectangle';
 
-  public color: 'red' | 'green' | 'blue';
-
-  private width: number;
-
-  private height: number;
-
-  constructor(color: 'red' | 'green' | 'blue', width: number, height: number) {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    private width: number,
+    private height: number,
+  ) {
     if (width <= 0 || height <= 0) {
       throw new Error('Width and height must be greater than 0');
     }
-
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   public getArea(): number {
